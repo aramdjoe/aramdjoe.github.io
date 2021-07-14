cb58c61f87fa908d00012717fb9ca8ad7607e433
aramdjoe <302523565@student.rocmondriaan.nl>
2021-07-14 22:27:17 +0200

    bijna klaar

--- JS/index.js
+++ JS/index.js
@@ -36,3 +36,28 @@ function myFunction() {
     var element = document.body;
     element.classList.toggle("dark-mode");
  }
+
+ let menuIcon = document.querySelector('.menuIcon');
+        let nav = document.querySelector('.overlay-menu');
+
+        menuIcon.addEventListener('click', () => {
+            if (nav.style.transform != 'translateX(0%)') {
+                nav.style.transform = 'translateX(0%)';
+                nav.style.transition = 'transform 0.2s ease-out';
+            } else { 
+                nav.style.transform = 'translateX(-100%)';
+                nav.style.transition = 'transform 0.2s ease-out';
+            }
+        });
+
+
+        // Toggle Menu Icon ========================================
+        let toggleIcon = document.querySelector('.menuIcon');
+
+        toggleIcon.addEventListener('click', () => {
+            if (toggleIcon.className != 'menuIcon toggle') {
+                toggleIcon.className += ' toggle';
+            } else {
+                toggleIcon.className = 'menuIcon';
+            }
+        });
